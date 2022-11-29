96d8783f69359285ab34178e4c3cceeb5ff6cc36
git-volt.software <info@volt.software>
2022-11-29 06:40:17 -0500

    method exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string? name;
+            string? color;
+            string? animal;
+            string? band;
+
+            //part one
+            
+            Console.WriteLine("What is your name? ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color? ");
+            color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal? ");
+            animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band? ");
+            band = "Stone Temple Pilots";
+
+            Console.WriteLine($"Your name is {name} and your favorite color is {color}. Also, your favorite animal is an {animal}. and your favorite band is {band}.");
+
+            //part two
+
+            Add(2,2);
+            Subtract(40,20);
+            Multiply(50,2);
+            Divide(100,2);
+            Modulus(9,3);
+
         }
+
+        static int Add(int x, int y){
+           return x + y;
+        }
+
+        static int Subtract(int x, int y){
+            return x - y;
+        }
+
+        static int Multiply(int x, int y){
+            return x * y;
+        }
+
+        static int Divide(int x, int y){
+            return x / y;
+        }
+
+        static int Modulus(int x, int y){
+            return x % y;
+        }
+
     }
 }
